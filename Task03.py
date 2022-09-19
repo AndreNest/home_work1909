0222d76c889038ebb9f2e0be19c391a26d085903
AndreyNesterov <1@nestandre.ru>
2022-09-19 20:30:24 +0300

    add Task03

---
+++ Task03.py
@@ -0,0 +1,17 @@
+file = open("name.txt", "r+", encoding="utf-8")
+list_name_students = []
+lines = file.readlines()
+for line in lines:
+    list_name_students.append(line.strip())
+def rename_student(new_list_name_studenst):
+    '''
+    Увелечение шрифта, в случае оценки более 4
+    '''
+    file.seek(0)
+    for i in range(len(new_list_name_studenst)):
+        if int(str(new_list_name_studenst[i])[len(new_list_name_studenst[i]) - 1]) > 4: # отсекаем оценки ниже 4
+            new_list_name_studenst[i] = str(new_list_name_studenst[i]).upper()
+        file.write('\n' + str(list_name_students[i]))
+    file.close()
+rename_student(list_name_students)
+
